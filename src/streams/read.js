f5005070d373613d90e93c2dda16c6c0229288bf
Ruslan <free.changer@gmail.com>
2022-12-04 16:09:10 +0000

    First commit for task-1

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+/**
+ * implement function that reads file fileToRead.txt content
+ * using Readable Stream 
+ * and prints it's content into process.stdout
+ */
+
+import { createReadStream } from "fs"
+import { URL } from "url"
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    const fileName = new URL('./files/fileToRead.txt', import.meta.url).pathname;
+
+    const fd = createReadStream(fileName);
+    fd.on('end', () => {
+        console.log()
+    })
+
+    fd.pipe(process.stdout);
 };
 
 await read();
+
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,22 @@
-// n should be received from main thread
+/**
+ * extend given function to work with data received from main thread
+ * and implement function which sends result of the computation
+ * to the main thread
+ */
+import { parentPort, workerData } from 'worker_threads'
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
+const sendResult = async () => {
     // This function sends result of nthFibonacci computations to main thread
+
+    await new Promise((resolve) => setTimeout(resolve, 10 * 1000));
+    console.log('sendResult::workerData', workerData)
+
+    const x = nthFibonacci(workerData)
+    console.log('sendResult::result', x)
+
+    parentPort.postMessage({ x })
 };
 
-sendResult();
+await sendResult();
